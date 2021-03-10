04d7b4ccf86d69513d88d253fa8853a5e11d4283
Marc Vaccaro <marcgvaccaro@gmail.com>
2021-03-09 23:08:46 -0800

    Add some initial styling

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Pagination } from './models/pagination';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private http: HttpClient) {
+  }
+  
+  ngOnInit(): void {
+    let get = this.http.get<Pagination>('https://localhost:5001/api/products')
+    get.subscribe((products: Pagination) => {
+      this.products = products.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+  
+ 
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
