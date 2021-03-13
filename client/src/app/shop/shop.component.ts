3747684d17b61e21df03ba87165a59a0c801e366
Marc Vaccaro <marcgvaccaro@gmail.com>
2021-03-13 13:25:26 -0800

    Add filtering to the frontend

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
+import { ProductBrand } from '../shared/models/productBrand';
+import { ProductType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,14 +11,53 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: Product[];
-  constructor(private shopService:ShopService) { }
+  brands: ProductBrand[];
+  types: ProductType[];
+  selectedBrandId: number = 0;
+  selectedTypeId: number = 0;
+
+
+  constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
-    this.shopService.getProducts().subscribe(res => {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
+    this.shopService.getProducts(this.selectedBrandId, this.selectedTypeId).subscribe(res => {
       this.products = res.data;
     }, error => {
       console.log(error);
     });
   }
 
+  getBrands() {
+    return this.shopService.getBrands().subscribe((brands: ProductBrand[]) => {
+      const allBrandsPlaceholder: ProductBrand = { id: 0, name: 'All' };
+      this.brands = [allBrandsPlaceholder, ...brands];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getTypes() {
+    return this.shopService.getTypes().subscribe((types: ProductType[]) => {
+      const allTypesPlaceholder: ProductType = { id: 0, name: 'All' };
+      this.types = [allTypesPlaceholder, ...types];
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  onBrandSelected(brandId: number) {
+    this.selectedBrandId = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId: number) {
+    this.selectedTypeId = typeId;
+    this.getProducts();
+  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,15 +1,38 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 import { Injectable } from '@angular/core';
 import { Pagination } from '../shared/models/pagination';
+import { ProductBrand } from '../shared/models/productBrand';
+import { ProductType } from '../shared/models/productType';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
-baseUrl = 'https://localhost:5001/api'
+  baseUrl = 'https://localhost:5001/api'
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get<Pagination>(`${this.baseUrl}/products`)
+  getProducts(brandId?: number, typeId?: number) {
+    let params = new HttpParams();
+    if (brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+    if (typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    return this.http.get<Pagination>(`${this.baseUrl}/products`, { observe: 'response', params })
+      .pipe(map(response => {
+        return response.body;
+      }));
+  }
+
+  getTypes() {
+    return this.http.get<ProductType[]>(`${this.baseUrl}/products/types`);
+  }
+
+  getBrands() {
+    return this.http.get<ProductBrand[]>(`${this.baseUrl}/products/brands`);
   }
 }
