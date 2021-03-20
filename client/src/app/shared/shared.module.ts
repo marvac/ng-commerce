87245e886ed37b55248227f82dfd8d9a89d4385b
Marc Vaccaro <marcgvaccaro@gmail.com>
2021-03-19 23:35:50 -0700

    Fix a lot of styles

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {PaginationModule} from 'ngx-bootstrap/pagination';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagingControlComponent } from './components/paging-control/paging-control.component';
 
-
 @NgModule({
   declarations: [
-    PagingHeaderComponent, 
+    PagingHeaderComponent,
     PagingControlComponent
   ],
   imports: [
