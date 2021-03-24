e6c9ca6de438728eddbb1d2777a79f47960fd297
Marc Vaccaro <marcgvaccaro@gmail.com>
2021-03-23 22:40:49 -0700

    Add basic components for checkout

--- client/src/app/checkout/checkout.module.ts
+++ client/src/app/checkout/checkout.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CheckoutComponent } from './checkout.component';
+import { CheckoutRoutingModule } from './checkout-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [CheckoutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CheckoutRoutingModule
   ]
 })
 export class CheckoutModule { }
