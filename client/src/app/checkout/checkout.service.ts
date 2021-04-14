4de1e26eb8aa22b0ca2f4e9f3441a9821dbad576
Marc Vaccaro <marcgvaccaro@gmail.com>
2021-04-13 22:26:54 -0700

    Add delivery method stepper to checkout

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { DeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
+baseUrl= environment.apiUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getDeliveryMethods(){
+    return this.http.get(`${this.baseUrl}/orders/deliveryMethods`).pipe(
+      map((deliveryMethods: DeliveryMethod[]) => {
+        return deliveryMethods.sort((a, b) => b.price - a.price);
+      })
+    );
+  }
 }
---
+++ client/src/app/shared/models/deliveryMethod.ts
@@ -0,0 +1,8 @@
+export interface DeliveryMethod {
+    id: number;
+    createdDate: Date;
+    shortName: string;
+    deliveryTime: string;
+    description: string;
+    price: number;
+}
